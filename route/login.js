c87f2adbff16934d8619f5662eae322e801f9213
Nisha Goel <nishagoel2001@gmail.com>
2023-04-07 14:48:09 +0530

    initial index commits

--- route/login.js
+++ route/login.js
@@ -1,13 +1,11 @@
-const express = require('express')
-const app = express()
-const session = require('express-session')
-const login_router = express.Router()
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const login_router = express.Router();
 
-const { login_render,login } = require('../controller/login')
+const { login_render, login } = require('../controller/login');
 
+login_router.get('/userLogin', login_render);
+login_router.post('/userLogin', login);
 
-login_router.get('/login', login_render);
-login_router.post('/login', login); 
-
-
-module.exports = login_router
+module.exports = login_router;
