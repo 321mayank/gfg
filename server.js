578519bd0d6723e024675d785f9aace0699dc9fe
Nisha Goel <nishagoel2001@gmail.com>
2023-04-07 15:31:30 +0530

    index changes and bootstrap package added

--- server.js
+++ server.js
@@ -2,22 +2,19 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
-const login_router = require('./route/login')
+const login_router = require('./route/login');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  //rendered the login page
+  res.render('index');
+});
 
+app.use('/', login_router);
 
-app.get('/', (req, res) => { //rendered the login page
-    res.render('index');
-})
-
-
-app.use('/',login_router)
-
-
+app.use(express.static('public'));
 
 app.listen(8000, () => {
-    console.log('Server listening on port 8000');
-  });
-  
+  console.log('Server listening on port 8000');
+});
